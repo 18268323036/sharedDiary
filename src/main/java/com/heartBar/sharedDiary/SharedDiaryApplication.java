c712fba64442402ed26d4dac6971086b0b60b317
zhangxy <232314690@qq.com>
2017-09-26 10:33:20 +0800

    sharedDiary
    Signed-off-by: zhangxy <232314690@qq.com>

---
+++ src/main/java/com/heartBar/sharedDiary/SharedDiaryApplication.java
@@ -0,0 +1,12 @@
+package com.heartBar.sharedDiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedDiaryApplication.class, args);
+	}
+}
